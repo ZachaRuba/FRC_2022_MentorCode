2c0249b444b497f80939bb7dfa03b0a2a5a10e64
ZachaRuba <zacharyjcanderson@gmail.com>
2022-02-24 21:51:55 -0800

    Added Autonomous Features
    Used autonomous functions

--- FRC_2022_MotorControllerExample/src/main/cpp/Robot.cpp
+++ FRC_2022_MotorControllerExample/src/main/cpp/Robot.cpp
@@ -6,6 +6,7 @@
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
+#include <time.h>
 
 /**
  * This sample program shows how to control a motor using a joystick. In the
@@ -18,15 +19,36 @@
 class Robot : public frc::TimedRobot {
  public:
   void TeleopPeriodic() override { 
-    left_motor.Set(m_controller.GetLeftY());
-    right_motor.Set(m_controller.GetRightY()); 
+    //left_motor.Set(m_controller.GetLeftY());
+    right_motor.Set(m_controller.GetLeftX()); 
     }
+
+  void AutonomousInit() override{
+    autoTimeStamp = time(NULL);
+  }
+
+  void AutonomousPeriodic() override{
+    if(autoTimeStamp + delay > time(NULL)){
+      right_motor.Set(maxSpeed);
+      left_motor.Set(-maxSpeed);
+    }
+    else if(autoTimeStamp + (delay * 2) > time(NULL)){
+      right_motor.Set(-maxSpeed);
+      left_motor.Set(maxSpeed);
+    }else{
+      right_motor.Set(0);
+      left_motor.Set(0);
+    }
+  }
 
  private:
   //frc::Joystick m_stick{0};
   frc::XboxController m_controller{0};
   frc::PWMSparkMax left_motor{0};
   frc::PWMSparkMax right_motor{1};
+  time_t autoTimeStamp;
+  time_t delay = 2;
+  double maxSpeed = 0.15;
 };
 
 #ifndef RUNNING_FRC_TESTS
